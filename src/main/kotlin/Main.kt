2d8f878fa507f121c43ebd1f17d60ec7975554d6
Francesco Losciale <francesco.losciale@mypulse.ai>
2024-03-01 13:25:41 +0000

    Add summary to verify 1 test successfully ran

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,12 +8,20 @@ open class TestCase(private val testMethodName: String) {
     open fun tearDown() {
         log += "tearDown "
     }
-    fun run() {
+    fun run(): TestResult {
+        val testResult = TestResult()
+        testResult.testStarted()
         setUp()
         val method: Method = this::class.java.getDeclaredMethod(this.testMethodName)
         method.invoke(this)
         tearDown()
+        return testResult
     }
+}
+
+class TestResult(private var runCount: Int = 0) {
+    fun summary() = "$runCount run, 0 failed"
+    fun testStarted() = runCount++
 }
 
 class WasRun(testMethodName: String): TestCase(testMethodName) {
@@ -28,8 +36,14 @@ class TestCaseTest(testMethodName: String) : TestCase(testMethodName) {
         test.run()
         assert(test.log == "setUp testMethod tearDown ")
     }
+    fun testResult() {
+        val test = WasRun("testMethod")
+        val result = test.run()
+        assert(result.summary() == "1 run, 0 failed")
+    }
 }
 
 fun main() {
     TestCaseTest("testTemplateMethod").run()
+    TestCaseTest("testResult").run()
 }
