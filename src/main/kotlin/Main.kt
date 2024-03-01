e2ab0a0c41f97cd4ea0186af569c32e99dcb9215
Francesco Losciale <francesco.losciale@mypulse.ai>
2024-03-01 13:01:11 +0000

    Add setUp capability

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,41 @@
 import java.lang.reflect.Method
 
 open class TestCase(private val testMethodName: String) {
+    var wasSetUp: Boolean = false
+    var wasRun: Boolean = false
+    open fun setUp() {
+        wasSetUp = true
+        wasRun = false
+    }
     fun run() {
+        setUp()
         val method: Method = this::class.java.getDeclaredMethod(this.testMethodName)
         method.invoke(this)
     }
 }
 
 class WasRun(testMethodName: String): TestCase(testMethodName) {
-    var wasRun: Boolean = false
-
     fun testMethod() {
         wasRun = true
     }
 }
 
 class TestCaseTest(testMethodName: String) : TestCase(testMethodName) {
+    lateinit var test: WasRun
+    override fun setUp() {
+        test = WasRun("testMethod")
+    }
     fun testRunning() {
-        val test = WasRun("testMethod")
-        assert(!test.wasRun)
         test.run()
         assert(test.wasRun)
+    }
+    fun testSetUp() {
+        test.run()
+        assert(test.wasSetUp)
     }
 }
 
 fun main() {
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
 }
